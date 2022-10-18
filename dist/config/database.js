deead913453bf7c97eba6e29872a3e55392f6049
Santhosh Umapathi <Santhosh.Umapathi@AppyMango.com>
2022-10-18 06:10:14 +0200

    Base project with supabase

---
+++ dist/config/database.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = ({ env }) => ({
+    connection: {
+        client: "postgres",
+        connection: {
+            host: env("DATABASE_HOST", "db.odxatggdesrbultueupi.supabase.co"),
+            port: env.int("DATABASE_PORT", 5432),
+            database: env("DATABASE_NAME", "postgres"),
+            user: env("DATABASE_USERNAME", "postgres"),
+            password: env("DATABASE_PASSWORD", "Da2xFyUGpgyjZPNH"),
+            ssl: {
+                rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+            },
+        },
+        debug: false,
+        useNullAsDefault: true,
+    },
+});
